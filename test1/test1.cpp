e887f4ca90e4f98ec94cbb1fc8b9fcbd0fe60e85
saipr <saipr@SAIPRIYA>
2025-06-14 08:28:47 +0530

    Add project files.

---
+++ test1/test1.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <memory>
+using namespace std;
+class A {
+public:
+	virtual void tree(){
+		cout << "base class" << endl;
+	}
+	void tree1() {
+		cout << "base class" << endl;
+	}
+};
+class B :public A {
+public:
+	void tree() {
+		cout << "derived class" << endl;
+	}
+	void tree1() {
+		cout << "derived class" << endl;
+	}
+};
+void main() {
+	unique_ptr<A> ptr(new B());
+	ptr->tree();
+	ptr->tree1();
+}
